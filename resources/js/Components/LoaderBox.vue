aa49fe304f165a56822725ef0203f878e4495859
d.parshakov <d.parshakov@platiza.ru>
2022-09-05 12:29:03 +0500

    loading component

---
+++ resources/js/Components/LoaderBox.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="xt-loader">
+        <div class='xt-loader-inner xt-loader-one'></div>
+        <div class='xt-loader-inner xt-loader-two'></div>
+        <div class='xt-loader-inner xt-loader-three'></div>
+    </div>
+</template>
