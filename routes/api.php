e1b6711f2a0dfcfaaed1b957487dbe1999bccd09
d.parshakov <d.parshakov@platiza.ru>
2022-09-06 17:11:24 +0500

    search game

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::get('/get_info', [\App\Http\Controllers\HomeController::class, 'info']);
 
 Route::get('/get_all_games', [\App\Http\Controllers\GameController::class, 'get_all_games']);
 Route::get('/get_game/{id}', [\App\Http\Controllers\GameController::class, 'get_game']);
+Route::get('/search/', [\App\Http\Controllers\GameController::class, 'search']);
 
 Route::get('/get_selections', [\App\Http\Controllers\SelectionController::class, 'get_selections']);
 Route::get('/get_selection/{id}', [\App\Http\Controllers\GameController::class, 'get_selection_games']);
