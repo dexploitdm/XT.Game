ce4ed9ec5bb7d776d305ad2ce8a643ad17ccb418
d.parshakov <d.parshakov@platiza.ru>
2022-09-07 12:47:41 +0500

    add icons

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
     plugins: [
@@ -20,6 +20,7 @@ export default defineConfig({
                 },
             },
         }),
+        Icons({  autoInstall: true, }),
         AutoImport({
             resolvers: [ElementPlusResolver()],
         }),
