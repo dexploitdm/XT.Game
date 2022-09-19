0f7565a8471d3b1442e6b21641e68a2a75a76310
d.parshakov <d.parshakov@platiza.ru>
2022-09-19 13:29:11 +0500

    clear

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -23,31 +23,5 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
 
         <GameList />
 
-
-        <div class="container bg-w" style="margin-top: 50px;">
-            <div>Игры</div>
-
-            <div class="buttons" style="max-width: 700px; margin: auto;">
-                <button class="xt-btn color-1">Приобрести</button>
-
-
-
-                <button class="xt-btn color-1">BUTTON</button>
-                <button class="xt-btn color-2">BUTTON</button>
-                <button class="xt-btn color-3">BUTTON</button>
-                <button class="xt-btn color-4">BUTTON</button>
-                <button class="xt-btn color-5">BUTTON</button>
-
-                <button class="xt-btn color-7">BUTTON</button>
-                <button class="xt-btn color-8">BUTTON</button>
-                <button class="xt-btn color-9">BUTTON</button>
-                <button class="xt-btn color-10">BUTTON</button>
-                <button class="xt-btn color-11">BUTTON</button>
-            </div>
-
-        </div>
-
-
-
     </BreezeMainLayout>
 </template>
