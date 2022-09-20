c7fdf7555d45e633dcd0788aa60cdee11ead2ed7
d.parshakov <d.parshakov@platiza.ru>
2022-09-20 11:54:27 +0500

    game single page

--- resources/js/Pages/game/id.vue
+++ resources/js/Pages/game/id.vue
@@ -1,37 +1,52 @@
 <script setup>
 import BreezeMainLayout from '@/Layouts/Main.vue';
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import SelectionList from '@/Components/selections/SelectionList.vue';
+import {Head, Link} from '@inertiajs/inertia-vue3';
 import {computed, onMounted, ref} from "vue";
-import { useStore } from 'vuex'
+import {useStore} from 'vuex'
+
 const store = useStore()
 
 const props = defineProps({
-    id: Number
+  id: String
 })
 
 const game = computed(() => store.getters.getGameItem)
 
 onMounted(async () => {
-    await store.dispatch("getGameItem", props.id);
+  await store.dispatch("getGameItem", props.id);
 })
 
 
 </script>
 
 <template>
-    <Head title="Game" />
-    <BreezeMainLayout>
-       this {{ props.id }}
+  <Head title="Game"/>
+  <BreezeMainLayout>
 
-        <div class="container bg-w" style="margin-top: 50px;">
-            <div>{{ game.title }}</div>
-<!--            <div :style="'background-image: url(/uploads/games/'+ game.cover +')'"></div>-->
-            <img :src="'/uploads/games/' + game.cover" style="max-width: 200px">
-            <div v-html="game.content"></div>
+    <div class="bg-w">
+      <div class="container">
+        <div class="container-box">
+
+          <div class="single-game">
+            <div class="single-game-cover">
+              <img :src="'/uploads/games/' + game.cover">
+            </div>
+            <div class="single-game-content">
+              <div class="single-game-content_title">{{ game.title }}</div>
+              <div class="single-game-content_desc" v-html="game.desc"></div>
+              <div class="single-game-content_text" v-html="game.content"></div>
+              <button type="button" class="xt-btn color-3">Приобрести</button>
+            </div>
+
+
+          </div>
 
         </div>
+      </div>
+    </div>
 
+    <SelectionList />
 
-
-    </BreezeMainLayout>
+  </BreezeMainLayout>
 </template>
