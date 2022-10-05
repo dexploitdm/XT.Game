cff045f96b2df3f0b9d3eb9e4445db785dbb20d2
d.parshakov <d.parshakov@platiza.ru>
2022-10-05 13:08:28 +0500

    optimization

--- resources/js/Components/Logo.vue
+++ resources/js/Components/Logo.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 <template>
     <div class="logo-box" :class="{ not_link: link }">
         <Link :href="route('home')">
-            <img class="logo-box-xg" src="/uploads/others/xt_logo.png">
+            <img class="logo-box-xg" src="/uploads/others/xt_logo.png" width="44" height="44" alt="Logo XT.Game">
             <div class="logo-box-text">XT.Game#999</div>
         </Link>
     </div>
