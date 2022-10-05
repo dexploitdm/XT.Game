a0ab6bc157b142276e6677b0fc34b47156136bda
d.parshakov <d.parshakov@platiza.ru>
2022-10-05 12:44:40 +0500

    remove list game

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,6 @@
 <script setup>
 import BreezeMainLayout from '@/Layouts/Main.vue';
 import SliderHome from '@/Components/slider/SliderHome.vue';
-import GameList from '@/Components/games/GameList.vue';
 import SelectionList from '@/Components/selections/SelectionList.vue';
 import GameCategories from '@/Components/games/GameCategory.vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
@@ -19,8 +18,6 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
         <SelectionList />
 
         <GameCategories />
-
-        <GameList />
 
     </BreezeMainLayout>
 </template>
