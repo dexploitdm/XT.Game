2f65d7fcb70eb9a2d20fd7671a808fc16fc8daff
d.parshakov <d.parshakov@platiza.ru>
2022-10-18 16:49:54 +0500

    screen home

---
+++ resources/js/Components/other/ScreenOne.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+<template>
+    <div class="container">
+        <div class="container-box">
+            <div class="x-screen">
+                <div class="x-screen-content">
+                    <h1>Покупка игр для xbox через Турцию</h1>
+                    <p>Какой нибудь текст</p>
+                    <ul>
+                        <li>Только цифровые ключи.</li>
+                        <li>Самые интересные предложения игр по скидкам всегда в наличии.</li>
+                        <li>Пожизненная гарантия на игры.</li>
+                        <li>Гарантия честности сделки.</li>
+                    </ul>
+                    <div class="ex-pixel"></div>
+                    <a class="xt-btn xt-btn__blue" href="http://exlv/login">Вход</a>
+                </div>
+                <div class="x-screen-cover">
+                    <picture>
+                        <source srcset="/images/gamepad.webp" type="image/webp">
+                        <img src="/images/gamepad.png" alt="gamepad" />
+                    </picture>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
