cc6fc87680a9ee7f533f09077fa459bc1f1e0625
d.parshakov <d.parshakov@platiza.ru>
2022-10-21 19:06:24 +0500

    add component for header

---
+++ resources/js/Components/other/Menu.vue
@@ -0,0 +1,69 @@
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3';
+import IconAccountBox from '~icons/el/shopping-cart-sign'
+import {computed, onMounted, ref} from "vue";
+import {useStore} from 'vuex'
+
+const store = useStore()
+
+const gameInCart = computed(() => store.getters.getCarts)
+
+let canLogin = ref([])
+let canRegister = ref([])
+
+onMounted(async () => {
+    getInfo()
+})
+
+const getInfo = async () => {
+    let response = await axios.get('/api/get_info');
+    canLogin.value = response.data.canLogin
+    canRegister.value = response.data.canRegister
+}
+</script>
+<template>
+    <div v-if="canLogin" class="header-content">
+        <div class="header-menu">
+            <div class="header-menu-auth">
+                <template  v-if="$page.props.auth.user">
+                    <Link :href="route('dashboard')" class="xt-btn color-5">
+                        Личный кабинет
+                    </Link>
+                    <Link :href="route('logout')" method="post" class="xt-btn color-10 lg:hidden">
+                        Выход
+                    </Link>
+                </template>
+
+                <template v-else>
+                    <Link :href="route('login')" class="xt-btn color-10 x-marr20">Вход</Link>
+                    <Link v-if="canRegister" :href="route('register')" class="xt-btn color-5">Регистрация</Link>
+                </template>
+            </div>
+        </div>
+
+        <div v-if="$page.props.auth.user" class="profile dropdown dropdown-end hidden lg:block">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                    <picture>
+                        <img src="/images/people2.webp" alt="avatar"/>
+                    </picture>
+                </div>
+            </label>
+            <ul tabindex="0"
+                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                    <a class="justify-between">
+                        Мои заказы
+                        <span class="badge">New</span>
+                    </a>
+                </li>
+                <li><a>Настройки</a></li>
+                <li>
+                    <Link :href="route('logout')" method="post">
+                        Выход
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,92 +1,27 @@
 <script setup>
 import Logo from '@/Components/Logo.vue';
-import {Link} from '@inertiajs/inertia-vue3';
+import Menu from '@/Components/other/Menu.vue';
+import Cart from '@/Components/other/Cart.vue';
 import IconAccountBox from '~icons/el/shopping-cart-sign'
-import {computed, onMounted, ref} from "vue";
-import {useStore} from 'vuex'
-
-const store = useStore()
-
-const gameInCart = computed(() => store.getters.getCarts)
-
-let canLogin = ref([])
-let canRegister = ref([])
-
-onMounted(async () => {
-    getInfo()
-})
-
-const getInfo = async () => {
-    let response = await axios.get('/api/get_info');
-    canLogin.value = response.data.canLogin
-    canRegister.value = response.data.canRegister
-}
-
 </script>
-
 <template>
     <header>
         <div class="navbar bg-base-100">
             <div class="flex-1">
                 <Logo/>
             </div>
-            <div v-if="canLogin" class="flex-none">
-                <div class="header-menu">
-                    <div class="header-menu-auth">
-                        <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="xt-btn color-5">
-                            Личный кабинет
-                        </Link>
-                        <template v-else>
-                            <Link :href="route('login')" class="xt-btn color-10 x-marr20">Вход</Link>
-                            <Link v-if="canRegister" :href="route('register')" class="xt-btn color-5">Регистрация</Link>
-                        </template>
+            <Cart />
+            <div class="hidden lg:block">
+                <Menu />
+            </div>
+            <div class="flex-none lg:hidden">
+                <div class="dropdown dropdown-end">
+                    <button class="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                    </button>
+                    <div tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <Menu />
                     </div>
-                </div>
-                <div class="cart dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle">
-                        <div class="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
-                            </svg>
-                            <span class="badge badge-sm indicator-item">{{ gameInCart.length }}</span>
-                        </div>
-                    </label>
-                    <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-                        <div class="card-body">
-                            <span class="font-bold">Количество продуктов: {{ gameInCart.length }}</span>
-                            <div class="card-actions">
-                                <Link :href="route('cart')">
-                                    <button class="xt-btn color-10">Посмотреть корзину</button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div v-if="$page.props.auth.user" class="profile dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
-                            <picture>
-                                <img src="/images/people2.webp" alt="avatar"/>
-                            </picture>
-                        </div>
-                    </label>
-                    <ul tabindex="0"
-                        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li>
-                            <a class="justify-between">
-                                Мои заказы
-                                <span class="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Настройки</a></li>
-                        <li>
-                            <Link :href="route('logout')" method="post">
-                                Выход
-                            </Link>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
