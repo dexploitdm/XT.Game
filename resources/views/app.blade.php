ab6b6eb87cb27dca241a9ad3ab5ae24aceee9122
d.parshakov <d.parshakov@platiza.ru>
2022-10-24 14:13:53 +0500

    replace theme color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Продажа игр для xbox через Турцию">
-        <meta name="theme-color" content="#333333"/>
+        <meta name="theme-color" content="#2bb673"/>
         <link rel="apple-touch-icon" href="{{ asset('favicon.PNG') }}">
         <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
