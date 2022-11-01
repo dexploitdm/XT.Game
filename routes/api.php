a45903d26dea340ba0a0ba92330589df3a32230b
d.parshakov <d.parshakov@platiza.ru>
2022-11-01 17:26:58 +0500

    fake payment api

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//TODO: Сделать только для авторизованых
+Route::post('/payment/{price}', [\App\Http\Controllers\OrderController::class, 'payment']);
+
+
 Route::get('/get_info', [\App\Http\Controllers\HomeController::class, 'info']);
 
 Route::get('/get_all_games', [\App\Http\Controllers\GameController::class, 'get_all_games']);
