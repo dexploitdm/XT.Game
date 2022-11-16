25ac0f7091b0057e49b82ac71475fc3b2afee7c2
d.parshakov <d.parshakov@platiza.ru>
2022-11-16 18:30:24 +0500

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Продажа игр для xbox через Турцию">
         <meta name="theme-color" content="#2bb673"/>
-        <link rel="apple-touch-icon" href="{{ asset('favicon.PNG') }}">
+        <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
         <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -18,4 +19,12 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function () {
+                navigator.serviceWorker.register('/sw.js');
+            });
+        }
+    </script>
 </html>
