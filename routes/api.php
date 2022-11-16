7952670106e1fc03dc4773d8bd706aff130a10a5
d.parshakov <d.parshakov@platiza.ru>
2022-11-16 17:26:31 +0500

    removal of excess

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::get('/get_info', [\App\Http\Controllers\HomeController::class, 'info']);
 Route::get('/get_all_games', [\App\Http\Controllers\GameController::class, 'get_all_games']);
 Route::get('/get_categories', [\App\Http\Controllers\GameController::class, 'get_categories']);
@@ -23,8 +19,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 
 Route::middleware(['is_admin'])->group(function () {
-    //Route::post('/add_game', [\App\Http\Controllers\GameController::class, 'add_game']);
-    //Route::get('/get_edit_game/{id}', [\App\Http\Controllers\GameController::class, 'get_edit_game']);
-    //Route::get('/update_game/{id}', [\App\Http\Controllers\GameController::class, 'update_game']);
     Route::get('/search_order/', [\App\Http\Controllers\Admin\OrderController::class, 'searchOrdersByUID']);
 });
