6b428108fdbea7036f3a1db0c553684a1d1d3123
d.parshakov <d.parshakov@platiza.ru>
2022-11-21 13:32:31 +0500

    update color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Продажа игр для xbox через Турцию">
-        <meta name="theme-color" content="#333333"/>
+        <meta name="theme-color" content="#1b1d1d"/>
         <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
         <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
         <link rel="manifest" href="{{ asset('/manifest.json') }}">
